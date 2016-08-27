f82f2de6512fb5bad9fa5b5871f8524982878196
Mateusz Tadeja <mateusz.tadeja@gmail.com>
2016-08-27 15:16:45 +0200

    1st

---
+++ NoweGG/NoweGG/Conversation.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoweGG
+{
+    class Conversation
+    {
+        public Conversation(Message msg)
+        {
+            Conv.Add(msg);
+            Usr1 = msg.To;
+            Usr2 = msg.From;
+        }
+
+        private User usr1;
+
+        public User Usr1
+        {
+            get
+            {
+                return usr1;
+            }
+            set
+            {
+                usr1 = value;
+            }
+        }
+
+        private User usr2;
+
+        public User Usr2
+        {
+            get
+            {
+                return usr2;
+            }
+            set
+            {
+                usr2 = value;
+            }
+        }
+
+        private List<Message> Conv = new List<Message>();
+    }
+
+    class Message
+    {
+        public Message(string txt, DateTime tim, User FR, User TO)
+        {
+            Text = txt;
+            Time = tim;
+            From = FR;
+            To = TO;
+        }
+
+        private string text;
+
+        public string Text
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                text = value;
+            }
+        }
+
+        private DateTime time;
+
+        public DateTime Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+
+        private User from;
+
+        public User From
+        {
+            get
+            {
+                return from;
+            }
+            set
+            {
+                from = value;
+            }
+        }
+
+        private User to;
+
+        public User To
+        {
+            get
+            {
+                return to;
+            }
+            set
+            {
+                to = value;
+            }
+        }
+    }
+}
---
+++ NoweGG/NoweGG/User.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoweGG
+{
+    class User
+    {
+        private bool active;
+
+        private string login;
+
+        private string password;
+
+        private int number = 0;
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        private int age = 0;
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+
+        private string city;
+
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+
+        private string country;
+
+        public string Country
+        {
+            get
+            {
+                return country;
+            }
+            set
+            {
+                country = value;
+            }
+        }
+
+        List<User> Book = new List<User>();
+    }
+}
